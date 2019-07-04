f3b28017f6f06978d8282126c9059268a1756172
Preston Smith <preston.s1993@gmail.com>
2019-07-03 20:41:31 -0500

    Scraper: Slight usage modification

--- src/rscminus/scraper/Scraper.java
+++ src/rscminus/scraper/Scraper.java
@@ -635,7 +635,7 @@ public class Scraper {
         System.out.println("options:");
         System.out.println("\t-h\t\t\tShow this usage dialog");
         System.out.println("\t-z\t\t\tProcess replays even if not authentic");
-        System.out.println("\t-s\t\t\tSanitize replays");
+        System.out.println("\t-s\t\t\tExport sanitized replays");
         System.out.println("\t-p\t\t\tSanitize public chat");
         System.out.println("\t-x\t\t\tSanitize private chat");
         System.out.println("\t-f\t\t\tSanitize friends and ignores");
